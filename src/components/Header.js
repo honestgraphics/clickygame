48140d15f62a9c68ff4ea09188c54c7372e6700a
honestgraphics <jeannie@honestgraphics.com>
2018-07-11 17:50:28 -0400

    changes to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -14,22 +14,24 @@ const checkCorrectState = (correct) => {
 }
 
 const Header = ({ correct, score, title, topScore }) => (
-  <nav className="navbar navbar-expand-lg navbar-dark bd-navbar fixed-top flex-items">
+  <nav className="navbar navbar-expand-lg navbar-dark bd-navbar fixed-top">
     <a className="navbar-brand flex-items" href="/">Clicky Game</a>
     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
 
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav ml-auto">
+      <ul className="navbar-nav mx-auto">
         {/* <li className="brand">
           <a className="navbar-brand flex-items" href="/">Clicky Game</a>
         </li> */}
         <li className={classNames(checkCorrectState(correct))}>
           {title}
         </li>
-        <li className="nav-item flex-item3">
-          Score: {score} | Top Score: {topScore}
+      </ul>
+      <ul className="nav navbar-nav">
+        <li className="nav-item">
+            Score: {score} | Top Score: {topScore}
         </li>
       </ul>
     </div>
